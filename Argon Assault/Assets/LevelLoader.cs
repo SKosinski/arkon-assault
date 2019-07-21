db04f8c2b96c9f4a703ca0bbcf6e554600d98108
SKosinski <stasiu.kosinski@hotmail.com>
2019-07-21 14:35:24 +0200

    section 1 done

---
+++ Argon Assault/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    public void LoadNextLevel()
+    {
+        Invoke("ApplyLoadNextLevel", levelLoadDelay);
+    }
+
+    public void ReloadLevel()
+    {
+        Invoke("ApplyReloadLevel", levelLoadDelay);
+    }
+
+    public void ApplyLoadNextLevel()
+    {
+        if (SceneManager.sceneCountInBuildSettings - 1 != SceneManager.GetActiveScene().buildIndex)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            SceneManager.LoadScene(0);
+    }
+
+    public void ApplyReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Argon Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        MusicPlayer[] players = FindObjectsOfType<MusicPlayer>();
+
+        if (players.Length>1)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
