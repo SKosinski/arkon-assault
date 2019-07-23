452b422e85e0e4939d6662202901d208056031b8
SKosinski <stasiu.kosinski@hotmail.com>
2019-07-23 17:22:10 +0200

    Section 3 done

---
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject enemyDeathFX;
+    [SerializeField] Transform parent;
+
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        FindObjectOfType<ScoreBoard>().AddScore();
+        Destroy();
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
